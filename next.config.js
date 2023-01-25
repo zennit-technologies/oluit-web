11959d28ed3dd8ffc66f40b3a02da559972554bb
zennit-technologies <89331752+zennit-technologies@users.noreply.github.com>
2023-01-25 18:30:14 +0530

    build dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const { hostname } = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`);
 const nextConfig = {
+  distDir: 'build',
   reactStrictMode: true,
   swcMinify: true,
   images: {
